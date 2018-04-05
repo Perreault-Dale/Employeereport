50bce51391efa893f4d944f9ed321c2ab9dfcabe
Dale Perreault <per15038@byui.edu>
2018-04-04 21:36:14 -0500

    Added WebView's to Team and Employee activities, pointing to temp locations

--- app/src/main/java/byui/cit360/employeereport/EmployeeActivity.java
+++ app/src/main/java/byui/cit360/employeereport/EmployeeActivity.java
@@ -2,6 +2,8 @@ package byui.cit360.employeereport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class EmployeeActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class EmployeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
+
+        WebView myWebView = findViewById(R.id.webView2);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.glassdoor.com");
     }
 }
--- app/src/main/java/byui/cit360/employeereport/TeamActivity.java
+++ app/src/main/java/byui/cit360/employeereport/TeamActivity.java
@@ -2,6 +2,8 @@ package byui.cit360.employeereport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class TeamActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class TeamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+
+        WebView myWebView = findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.espn.com");
     }
 }
